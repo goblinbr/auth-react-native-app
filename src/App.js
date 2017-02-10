499f83fc14d3988db6738c10e5d433fc145659b5
Rodrigo de Bona Sartor <rodrigo@kugel.com.br>
2017-02-10 14:09:08 -0200

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { Header } from './components';
 
 class App extends Component {
   render() {
     return (
       <View>
+        <Header text="Auth app" />
         <Text>
           An App!
         </Text>
---
+++ src/components/styles.js
@@ -0,0 +1,45 @@
+const styles = {
+  card: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 2,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10
+  },
+  cardSection: {
+    borderBottomWidth: 1,
+    padding: 5,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    borderColor: '#ddd',
+    position: 'relative'
+  },
+  headerView: {
+    backgroundColor: '#F0F0F0',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  },
+  headerText: {
+    fontSize: 20
+  },
+  buttonContainer: {
+    flex: 1,
+    alignSelf: 'stretch',
+  }
+};
+
+export default styles;
